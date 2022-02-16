cf4ea25debc82c605e59d193d382a39161fd7629
Alex <alexis.amand@gmail.com>
2022-02-16 15:51:07 +0100

    [bugfix] perso qui ne restait pas sur les plateformes qui bougent

--- Game 2D/Assets/Scripts/Game/MovingPlatform.cs
+++ Game 2D/Assets/Scripts/Game/MovingPlatform.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Ce script est en partie adapté de celui qui s'occupe du déplacemennt des ennemis */
+
 public class MovingPlatform : MonoBehaviour
 {
-    public float speed; /* vitesse de déplacement de l'ennemi */
-    public Transform[] waypoints; /* liste de points vers lesquels l'ennemi doit se déplacer */
+    public float speed; /* vitesse de déplacement de la plateforme */
+    public Transform[] waypoints; /* liste de points vers lesquels la plateforme doit se déplacer */
 
-    private Transform target; /* Cible vers laquelle l'ennemi va se déplacer */
+    private Transform target; /* Cible vers laquelle la plateforme va se déplacer */
     private int destPoint = 0; /* le point de destination, sorte d'index */
 
     public GameObject Player;
@@ -22,13 +24,13 @@ public class MovingPlatform : MonoBehaviour
     void Update()
     {
 
-    /* Dans quelle direction faut-il déplacer l'ennemi ? */
-    Vector3 dir = target.position - transform.position;
+        /* Dans quelle direction faut-il déplacer la plateforme ? */
+        Vector3 dir = target.position - transform.position;
 
-        /* déplacement de l'ennemi */
+        /* déplacement de la plateforme */
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-        /* Si l'ennemi est quasiment arrivé à sa destination */
+        /* Si la plateforme est quasiment arrivé à sa destination */
         if (Vector3.Distance(transform.position, target.position) < 0.3f)
         {
             destPoint = (destPoint + 1) % waypoints.Length;
@@ -37,5 +39,24 @@ public class MovingPlatform : MonoBehaviour
 
     }
 
-}
+    /* Les deux fonctions qui suivent corrigent le bug du perso sur la plateforme
+     * Il reste dessus et la suit, plus besoin d'appyer sur gauche/droite pour maintenir
+     * le personnage dessus */
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(this.transform);
+        }
+    }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+
+}
